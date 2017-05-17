0cf61f6062f669053c4c5ea48f470122e857b38f
Anthony <anthony.cesnik@gmail.com>
2017-05-17 00:26:34 -0500

    fixes

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Genomics;
 using GenomicsData;
 
 namespace Benchmark
@@ -11,7 +12,9 @@ namespace Benchmark
     {
         private static void Main(string[] args)
         {
-            VCF vcf = new VCF(@"E:\lelantos\linux_docs\TenCellLines\VCFs");
+            VCF vcf = new VCF(@"E:\lelantos\linux_docs\TenCellLines\VCFs\A549.vcf");
+            Console.WriteLine(vcf.samples[0].sequence_variants.Count);
+            Console.ReadKey();
         }
     }
 }
