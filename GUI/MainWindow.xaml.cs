57348c65f0ea0e6819434cf7c016f5969bb06c29
lonelu <lonelur@gmail.com>
2018-05-15 14:29:30 -0500

    Add StarAlignmentWorkFlowWindows

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.IO;
+using System.Reflection;
+using System.Collections.ObjectModel;
 using WorkflowLayer;
+
 
 namespace SpritzGUI
 {
@@ -15,11 +19,17 @@ namespace SpritzGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Fields
+
         private readonly ObservableCollection<GenomeFastaDataGrid> genomeFastaCollection = new ObservableCollection<GenomeFastaDataGrid>();
         private readonly ObservableCollection<GeneSetDataGrid> geneSetCollection = new ObservableCollection<GeneSetDataGrid>();
         private readonly ObservableCollection<RNASeqFastqDataGrid> rnaSeqFastqCollection = new ObservableCollection<RNASeqFastqDataGrid>();
         private ObservableCollection<InRunTask> dynamicTasksObservableCollection;
         private readonly ObservableCollection<PreRunTask> staticTasksObservableCollection = new ObservableCollection<PreRunTask>();
+
+        #endregion Private Fields
+
+        #region Public Constructors
 
         public MainWindow()
         {
@@ -32,6 +42,10 @@ namespace SpritzGUI
 
             EverythingRunnerEngine.NewRnaSeqFastqHandler += AddNewRnaSeqFastq;
         }
+
+        #endregion Public Constructors
+
+        #region Private Methods - Controlers
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
@@ -56,14 +70,27 @@ namespace SpritzGUI
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+
         }
 
-        private void MenuItem_Help_Click(object sender, RoutedEventArgs e)
+        private void MenuItem_Wiki_Click(object sender, RoutedEventArgs e)
         {
+            System.Diagnostics.Process.Start(@"https://github.com/smith-chem-wisc/Spritz/wiki");
         }
 
         private void MenuItem_Contact_Click(object sender, RoutedEventArgs e)
         {
+            System.Diagnostics.Process.Start(@"https://github.com/smith-chem-wisc/Spritz");
+        }
+
+        private void BtnSTARAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new STARAlignWorkFlowWindows();
+            if (dialog.ShowDialog() == true)
+            {
+                AddTaskToCollection(dialog.TheTask);
+                UpdateTaskGuiStuff();
+            }
         }
 
         private void BtnAddFastq2Proteins_Click(object sender, RoutedEventArgs e)
@@ -71,6 +98,8 @@ namespace SpritzGUI
             var dialog = new Fastq2ProteinsFlowWindows();
             if (dialog.ShowDialog() == true)
             {
+                //AddTaskToCollection(dialog.TheTask);
+                UpdateTaskGuiStuff();
             }
         }
 
@@ -79,23 +108,8 @@ namespace SpritzGUI
             var dialog = new LncRNADiscoverFlowWindows();
             if (dialog.ShowDialog() == true)
             {
-            }
-        }
-
-        private void UpdateTaskGuiStuff()
-        {
-            if (staticTasksObservableCollection.Count == 0)
-            {
-                RunWorkflowButton.IsEnabled = false;
-                RemoveLastTaskButton.IsEnabled = false;
-                ClearTasksButton.IsEnabled = false;
-                ResetTasksButton.IsEnabled = false;
-            }
-            else
-            {
-                RunWorkflowButton.IsEnabled = true;
-                RemoveLastTaskButton.IsEnabled = true;
-                ClearTasksButton.IsEnabled = true;
+                AddTaskToCollection(dialog.TheTask);
+                UpdateTaskGuiStuff();
             }
         }
 
@@ -107,7 +121,7 @@ namespace SpritzGUI
                 dynamicTasksObservableCollection.Add(new InRunTask("Workflow" + (i + 1) + "-" + staticTasksObservableCollection[i].spritzWorkflow.WorkflowType.ToString(), staticTasksObservableCollection[i].spritzWorkflow));
             }
             workflowTreeView.DataContext = dynamicTasksObservableCollection;
-            EverythingRunnerEngine a = new EverythingRunnerEngine(dynamicTasksObservableCollection.Select(b => new Tuple<string, SpritzFlow>(b.DisplayName, b.workflow)).ToList(), rnaSeqFastqCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), genomeFastaCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), geneSetCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), OutputFolderTextBox.Text);
+            EverythingRunnerEngine a = new EverythingRunnerEngine(dynamicTasksObservableCollection.Select(b => new Tuple<string, SpritzFlow>(b.DisplayName, b.workflow)).ToList(), genomeFastaCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), geneSetCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), rnaSeqFastqCollection.Where(b => b.Use).Select(b => b.FilePath).ToList(), OutputFolderTextBox.Text);
             var t = new Task(a.Run);
             t.Start();
         }
@@ -187,45 +201,38 @@ namespace SpritzGUI
             geneSetCollection.Clear();
         }
 
-        private void AddAFile(string filepath)
-        {
-            var theExtension = Path.GetExtension(filepath).ToLowerInvariant();
-            switch (theExtension)
-            {
-                case ".fa":
-                    GenomeFastaDataGrid genomeFasta = new GenomeFastaDataGrid(filepath);
-                    genomeFastaCollection.Add(genomeFasta);
-                    break;
-
-                case ".gtf":
-                case ".gff3":
-                    GeneSetDataGrid geneSet = new GeneSetDataGrid(filepath);
-                    geneSetCollection.Add(geneSet);
-                    break;
-
-                case ".fastq":
-                case ".fastq.gz":
-                    RNASeqFastqDataGrid rnaSeqFastq = new RNASeqFastqDataGrid(filepath);
-                    rnaSeqFastqCollection.Add(rnaSeqFastq);
-                    UpdateOutputFolderTextbox();
-                    break;
-            }
-        }
-
         private void LoadTaskButton_Click(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.OpenFileDialog openPicker = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "TOML files(*.toml)|*.toml",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+                Multiselect = true
+            };
+            if (openPicker.ShowDialog() == true)
+                foreach (var tomlFromSelected in openPicker.FileNames)
+                {
+                    AddAFile(tomlFromSelected);
+                }
+            UpdateTaskGuiStuff();
         }
 
         private void ClearTasksButton_Click(object sender, RoutedEventArgs e)
         {
+            staticTasksObservableCollection.Clear();
+            UpdateTaskGuiStuff();
         }
 
         private void RemoveLastTaskButton_Click(object sender, RoutedEventArgs e)
         {
+            staticTasksObservableCollection.RemoveAt(staticTasksObservableCollection.Count - 1);
+            UpdateTaskGuiStuff();
         }
 
         private void ResetTasksButton_Click(object sender, RoutedEventArgs e)
         {
+
         }
 
         private void AddNewRnaSeqFastq(object sender, StringListEventArgs e)
@@ -246,6 +253,39 @@ namespace SpritzGUI
             }
         }
 
+        private void MenuItem_Setup_Click(object sender, RoutedEventArgs e)
+        {
+            ManageToolsFlow.Install(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+        }
+
+        #endregion Private Methods - Controlers
+
+        #region Private Methods - no Controlers
+
+        private void UpdateTaskGuiStuff()
+        {
+            if (staticTasksObservableCollection.Count == 0)
+            {
+                RunWorkflowButton.IsEnabled = false;
+                RemoveLastTaskButton.IsEnabled = false;
+                ClearTasksButton.IsEnabled = false;
+                ResetTasksButton.IsEnabled = false;
+            }
+            else
+            {
+                RunWorkflowButton.IsEnabled = true;
+                RemoveLastTaskButton.IsEnabled = true;
+                ClearTasksButton.IsEnabled = true;
+            }
+        }
+
+        private void AddTaskToCollection(SpritzFlow ye)
+        {
+            PreRunTask te = new PreRunTask(ye);
+            staticTasksObservableCollection.Add(te);
+            staticTasksObservableCollection.Last().DisplayName = "Task" + (staticTasksObservableCollection.IndexOf(te) + 1);
+        }
+
         private void UpdateOutputFolderTextbox()
         {
             if (rnaSeqFastqCollection.Any())
@@ -263,5 +303,33 @@ namespace SpritzGUI
                 OutputFolderTextBox.Clear();
             }
         }
+
+        private void AddAFile(string filepath)
+        {
+            var theExtension = Path.GetExtension(filepath).ToLowerInvariant();
+            switch (theExtension)
+            {
+                case ".fa":
+                    GenomeFastaDataGrid genomeFasta = new GenomeFastaDataGrid(filepath);
+                    genomeFastaCollection.Add(genomeFasta);
+                    break;
+                case ".gtf":
+                case ".gff3":
+                    GeneSetDataGrid geneSet = new GeneSetDataGrid(filepath);
+                    geneSetCollection.Add(geneSet);
+                    break;
+                case ".fastq":
+                case ".fastq.gz":
+                    RNASeqFastqDataGrid rnaSeqFastq = new RNASeqFastqDataGrid(filepath);
+                    rnaSeqFastqCollection.Add(rnaSeqFastq);
+                    UpdateOutputFolderTextbox();
+                    break;
+            }
+        }
+
+
+
+        #endregion Private Methods - no Controlers
+
     }
 }
---
+++ GUI/STARAlignWorkFlowWindows.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WorkflowLayer;
+
+namespace SpritzGUI
+{
+    /// <summary>
+    /// Interaction logic for STARAlignWorkFlowWindows.xaml
+    /// </summary>
+    public partial class STARAlignWorkFlowWindows : Window
+    {
+        public STARAlignWorkFlowWindows()
+        {
+            InitializeComponent();
+        }
+
+        internal STARAlignmentFlow TheTask { get; private set; }
+
+        protected void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        protected void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
