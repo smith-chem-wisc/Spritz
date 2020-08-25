ab6cbf142ad132143205dacdf13b3666ece367f5
acesnik <acesnik@chem.wisc.edu>
2020-08-24 19:46:28 -0500

    remove nonascii chars

--- Spritz/scripts/prose.py
+++ Spritz/scripts/prose.py
@@ -28,7 +28,7 @@ lines.extend([
     "- https://academic.oup.com/bioinformatics/article/34/17/i884/5093234",
     "",
     "Reads are aligned using hisat2: ",
-    "- Kim, D.; et al. Graph-Based Genome Alignment and Genotyping with HISAT2 and HISAT-Genotype. Nat. Biotechnol. 2019, 37 (8), 907–915. https://doi.org/10.1038/s41587-019-0201-4.",
+    "- Kim, D.; et al. Graph-Based Genome Alignment and Genotyping with HISAT2 and HISAT-Genotype. Nat. Biotechnol. 2019, 37 (8), 907-915. https://doi.org/10.1038/s41587-019-0201-4.",
     "",
     "Alignments are analyzed and combined using samtools: ",
     "- https://academic.oup.com/bioinformatics/article/25/16/2078/204688",
@@ -39,19 +39,19 @@ lines.extend([
 if "variant" in workflows:
     lines.extend([
         "Alignments are prepared for variant calling and analyzed for variants using the Genome Analysis Toolkit (GATK): ",
-        "- McKenna, A.; et al. The Genome Analysis Toolkit: A MapReduce Framework for Analyzing next-Generation DNA Sequencing Data. Genome Res. 2010, 20 (9), 1297–1303. https://doi.org/10.1101/gr.107524.110.",
-        "- DePristo, M. A.; et al. A Framework for Variation Discovery and Genotyping Using Next-Generation DNA Sequencing Data. Nat. Genet. 2011, 43 (5), 491–498. https://doi.org/10.1038/ng.806.",
+        "- McKenna, A.; et al. The Genome Analysis Toolkit: A MapReduce Framework for Analyzing next-Generation DNA Sequencing Data. Genome Res. 2010, 20 (9), 1297-1303. https://doi.org/10.1101/gr.107524.110.",
+        "- DePristo, M. A.; et al. A Framework for Variation Discovery and Genotyping Using Next-Generation DNA Sequencing Data. Nat. Genet. 2011, 43 (5), 491-498. https://doi.org/10.1038/ng.806.",
         "- Poplin, R.; et al. Scaling Accurate Genetic Variant Discovery to Tens of Thousands of Samples; preprint; Genomics, 2017. https://doi.org/10.1101/201178.",
         "",
         "SnpEff is used for variant annotation and customized in Spritz to output a proteogenomic database: ",
-        "- Cingolani, P.; et al. A Program for Annotating and Predicting the Effects of Single Nucleotide Polymorphisms, SnpEff: SNPs in the Genome of Drosophila Melanogaster Strain W1118; Iso-2; Iso-3. Fly (Austin) 2012, 6 (2), 80–92. https://doi.org/10.4161/fly.19695.",
+        "- Cingolani, P.; et al. A Program for Annotating and Predicting the Effects of Single Nucleotide Polymorphisms, SnpEff: SNPs in the Genome of Drosophila Melanogaster Strain W1118; Iso-2; Iso-3. Fly (Austin) 2012, 6 (2), 80-92. https://doi.org/10.4161/fly.19695.",
         ""])
 
 # ISOFORM ANALYSIS
 if "isoform" in workflows:
     lines.extend([
         "Isoform analysis is performed using a pipeline from ProteomeGenerator: ",
-        "- Cifani, P.; et al. ProteomeGenerator: A Framework for Comprehensive Proteomics Based on de Novo Transcriptome Assembly and High-Accuracy Peptide Mass Spectral Matching. J. Proteome Res. 2018, 17 (11), 3681–3692. https://doi.org/10.1021/acs.jproteome.8b00295.",
+        "- Cifani, P.; et al. ProteomeGenerator: A Framework for Comprehensive Proteomics Based on de Novo Transcriptome Assembly and High-Accuracy Peptide Mass Spectral Matching. J. Proteome Res. 2018, 17 (11), 3681-3692. https://doi.org/10.1021/acs.jproteome.8b00295.",
         ""])
 
 with open(outf, 'w') as file:
