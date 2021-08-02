17c5829543916cc53aabe3e648705bdbaca8c569
acesnik <acesnik@chem.wisc.edu>
2021-08-02 07:44:26 -0500

    fix login and upload

---
+++ Spritz/SpritzTest/SpritzTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using SpritzModifications;
+using System;
+
+namespace SpritzTest
+{
+    public class SpritzTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var mods = ProteinAnnotation.GetUniProtMods(Environment.CurrentDirectory);
+            Assert.Greater(mods.Count, 0);
+        }
+    }
+}
