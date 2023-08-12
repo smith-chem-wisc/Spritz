3933f13c2d6a8ff7757ffb6bdf1b924ab303909a
Anthony Cesnik <cesnik@stanford.edu>
2023-08-12 13:22:24 -0700

    fix download workflow

--- Spritz/workflow/scripts/download_uniprot.py
+++ Spritz/workflow/scripts/download_uniprot.py
@@ -1,5 +1,3 @@
-# see https://www.ebi.ac.uk/training/online/sites/ebi.ac.uk.training.online/files/UniProt_programmatically_py3.pdf
-
 import get_proteome
 import requests
 import sys
@@ -7,22 +5,17 @@ import sys
 format = sys.argv[1]
 
 proteome = get_proteome.proteome
-BASE = 'http://legacy.uniprot.org'
-KB_ENDPOINT = '/uniprot/'
-TOOL_ENDPOINT = '/uploadlists/'
-
-# query = 'name:"polymerase alpha" AND proteome:UP000005640 AND reviewed:yes'
-# query = 'proteome:UP000005640 AND reviewed:yes'
-query = 'proteome:' + proteome
+BASE_URL = 'https://rest.uniprot.org'
+ENDPOINT = '/uniprot/search'
 
 payload = {
-    'query': query,
+    'query': proteome,
     'format': format,
     'include': 'yes', # include isoforms in fasta
     # 'columns': 'id,entry_name,reviewed,protein_names,organism,ec,keywords',
     }
 
-result = requests.get(BASE + KB_ENDPOINT, params=payload, stream=True)
+result = requests.get(BASE_URL + ENDPOINT, params=payload, stream=True)
 result.raise_for_status() # throw an error for bad status code
 for block in result.iter_content(1024):
     sys.stdout.buffer.write(block)
--- Spritz/workflow/scripts/get_proteome.py
+++ Spritz/workflow/scripts/get_proteome.py
@@ -5,9 +5,13 @@ import sys
 import yaml
 
 # find proteome
-BASE = 'http://legacy.uniprot.org'
-KB_ENDPOINT = '/proteomes/'
-TOOL_ENDPOINT = '/uploadlists/'
+BASE_URL = 'https://rest.uniprot.org'
+ENDPOINT = '/proteomes/search'
+
+params = {
+    'query': '*',
+    'format': 'tsv',
+}
 
 with open("config/config.yaml", 'r') as stream:
    data = yaml.safe_load(stream)
@@ -19,19 +23,18 @@ organism = data["organism"].lower()
 if query == 'canis_familiaris':
     query = 'canis_lupus_familiaris'
 
-payload = {
-    'query': query,
-    'sort': 'score',
-    'format': 'tab',
-    }
 
-proteome_res = requests.get(BASE + KB_ENDPOINT, params=payload, stream=True)
+proteome_res = requests.get(BASE_URL + ENDPOINT, params=params, stream=True)
 proteome_res.raise_for_status() # throw an error for bad status code
 
 results = proteome_res.text.split('\n')[1:]
 
 for r in results:
     splt = r.split('\t')
-    if organism in splt[1].lower():
+    if organism.replace('_', ' ') in splt[1].lower():
         proteome = splt[0]
         break
+
+if proteome is None:
+    print(f"Proteome for organism {organism} not found.")
+    sys.exit(1)
